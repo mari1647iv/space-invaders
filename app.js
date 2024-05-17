cfdb385ea83c20a8724c05a51dee5f023caccc61
mari1647iv <ima1647@outlook.com>
2024-05-17 03:14:40 +0300

    feat: add different shapes of invaders formation

--- app.js
+++ app.js
@@ -31,11 +31,7 @@ init();
 // *** utils
 // /*** *** game flow
 function init() {
-    invaders = [
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-        15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-        30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-    ];
+    invaders = shapes[Math.floor(Math.random() * shapes.length)]
     spaceship = width ** 2 - Math.ceil(width / 2);
 
     draw();
